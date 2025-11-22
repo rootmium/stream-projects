5eb2aeb00231eae683594fb8172c135bf0aa44e9
rootmium <rootmium@gmail.com>
2025-11-23 00:52:04 +0600

    adding middleware for parsing json requests

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+// middleware for json request parsing
+app.use(express.json());
+
 // route root
 app.get("/", async (_req, res) => {
   res.json({ message: "Hello world!" });
