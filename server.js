d8cbfdc8ebd8c813d3f7ea2f45421c7fbf7d77ce
rootmium <rootmium@gmail.com>
2025-11-23 00:33:45 +0600

    adding root route

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ console.log("server file is great!");
 import express from "express";
 const app = express();
 
+// route root
+app.get("/", async (req, res) => {
+  res.send("Hello world!");
+});
+
 // Start listening
 app.listen(3000, () => {
   console.log("Server is running at http://localhost:3000");
