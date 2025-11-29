cb2f1e3f6d112a17419c206c70e981ab372836ed
rootmium <rootmium@gmail.com>
2025-11-30 01:00:05 +0600

    feat: adding first route and method handling

--- a-003-http-server/index.ts
+++ a-003-http-server/index.ts
@@ -3,14 +3,17 @@ import { createServer } from "node:http";
 const port = 3000;
 
 const server = createServer((req, res) => {
-  const url = `http://${req.headers.host}${req.url}`
-  const parsedUrl = new URL(url)
-  console.log(parsedUrl)
+  const { method , url } = req
+  const parsedUrl = new URL(url, `http://${req.headers.host}`)
+  const path = parsedUrl.pathname
 
-  res.writeHeader(201, {
-    "Content-Type": "text/json"
-  })
-  res.end(JSON.stringify({ name: "hello world!" }))
+  if (path === "/" && method === "GET") {
+    res.writeHeader(200, { "Content-Type": "text/json" })
+    res.end(JSON.stringify({ name: "hello world!" }))
+  } else {
+    res.writeHeader(404, { "Content-Type": "text/json" })
+    res.end(JSON.stringify({ erro: "Wrong url" }))
+  }
 });
 
 server.listen(port, () => {
